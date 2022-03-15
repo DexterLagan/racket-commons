dfcd22998ee922bea6af737f0c3f5424dcacfecd
Dexter Santucci <38226103+DexterLagan@users.noreply.github.com>
2022-03-15 06:25:29 -0400

    Update commons.rkt

--- commons.rkt
+++ commons.rkt
@@ -1,32 +1,37 @@
 #lang racket
 (require racket/date)
-(provide chop                       ; (chop l n)
-         echo                       ; (echo ... ... ...)
-         define-command-line-params ; (define-command-line-params appname param1 ...)
-         list-to-string-lines       ; (list-to-string-lines l)
-         string-nth                 ; (string-nth str nth [sep #px"\\s+"])
-         mask                       ; (mask l1 l2)
-         mask-not                   ; (mask-not l1 l2)
-         non-empty-list?            ; (non-empty-list? l)
-         non-empty-same-length?     ; (non-empty-same-length? l1 l2 l3 ...)
-         currym                     ; ((currym func param1 param3) param2) (func param1 param2 param3)
-         curry-ref                  ; (curry-ref id params ref)
-         get-matching-seconds       ; (get-matching-seconds lst key)
-         string-replace-list        ; (string-replace-list source pattern-list destination)
-         multi-replace-line         ; (multi-replace-line line source-list destination)
-         multi-replace-lines        ; (multi-replace-lines lines source-list destination)
-         license-expired?           ; (license-expired? license-year)
-         license-almost-expired?    ; (license-almost-expired? license-month)
-         print-list                 ; (print-list l)
-         list-of-one?               ; (list-of-one? l)
-         ++                         ; (++ ... ... ...)
-         zip                        ; (zip l1 l2)
-         second-last                ; (second-last l)
-         replace-filename-in-path   ; (replace-filename-in-path full-path new-filename)
-         first-letter               ; (first-letter str)
-         if-defined                 ; (if-defined some-symbol 'defined 'not defined)
-         write-log                  ; (write-log str1 str2 ...)
-         combine-with)              ; (combine-with f l1 l2)
+(provide chop                            ; (chop l n)
+         echo                            ; (echo ... ... ...)
+         define-command-line-params      ; (define-command-line-params appname param1 ...)
+         list-to-string-lines            ; (list-to-string-lines l)
+         string-nth                      ; (string-nth str nth [sep #px"\\s+"])
+         mask                            ; (mask l1 l2)
+         mask-not                        ; (mask-not l1 l2)
+         non-empty-list?                 ; (non-empty-list? l)
+         non-empty-same-length?          ; (non-empty-same-length? l1 l2 l3 ...)
+         currym                          ; ((currym func param1 param3) param2) (func param1 param2 param3)
+         curry-ref                       ; (curry-ref id params ref)
+         get-matching-seconds            ; (get-matching-seconds lst key)
+         string-replace-list             ; (string-replace-list source pattern-list destination)
+         multi-replace-line              ; (multi-replace-line line source-list destination)
+         multi-replace-lines             ; (multi-replace-lines lines source-list destination)
+         license-expired?                ; (license-expired? license-year)
+         license-almost-expired?         ; (license-almost-expired? license-month)
+         print-list                      ; (print-list l)
+         list-of-one?                    ; (list-of-one? l)
+         ++                              ; (++ ... ... ...)
+         zip                             ; (zip l1 l2)
+         second-last                     ; (second-last l)
+         replace-filename-in-path        ; (replace-filename-in-path full-path new-filename)
+         first-letter                    ; (first-letter str)
+         if-defined                      ; (if-defined some-symbol 'defined 'not defined)
+         write-log                       ; (write-log str1 str2 ...)
+         take-everything-until-including ; (take-everything-until-including l starts-with)
+         take-everything-starts-with     ; (take-everything-starts-with l prefix)
+         take-everything-after-including ; (take-everything-after-including l starts-with)
+         get-clipboard-text              ; (get-clipboard-text)
+         set-clipboard-text              ; (set-clipboard-text s)
+         combine-with)                   ; (combine-with f l1 l2)
 
 (module+ test
   (require rackunit))
@@ -307,5 +312,25 @@
 (module+ test
   (check-equal? (combine-with string-append '("a" "b" "c") '("d" "e" "f")) '("ad" "be" "cf")))
 
+; returns the contents of the cliboard as text
+(define (get-clipboard-text)
+  (send the-clipboard get-clipboard-string 0))
+
+; set the contents of the cliboard as text
+(define (set-clipboard-text s)
+  (send the-clipboard set-clipboard-string s 0))
+  
+;; returns all the lines, including the one starting with, drop the rest
+(define (take-everything-until-including l starts-with)
+  (dropf-right l (λ (s) (not (string-prefix? s starts-with)))))
+
+;; returns all the lines that start with the given prefix
+(define (take-everything-starts-with l prefix)
+  (filter (λ (s) (string-prefix? s prefix)) l))
+
+;; returns all the lines after the line that starts with given prefix
+(define (take-everything-after-including l starts-with)
+  (dropf l (λ (s) (not (string-prefix? s starts-with)))))
+  
 
 ; EOF
