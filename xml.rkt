ec6956a851344815c6aaed4637b1076dbc46795a
Dexter Santucci <38226103+DexterLagan@users.noreply.github.com>
2022-03-15 06:45:55 -0400

    Create xml.rkt

---
+++ xml.rkt
@@ -0,0 +1,37 @@
+#lang racket
+(provide get-xml-value-from-id) ; (get-xml-value-from-id file pattern)
+
+;;; purpose
+
+; to provide a useful collection of functions for handling XML and XML files.
+
+;;; version history
+
+; v1.0 - this version.
+
+;;; defs
+
+;; returns the value of an XML line given its id (harmony format with value before id)
+;; returns the username given the contents of a localprefs.xml file
+;; <string value="usabatch" id="LAST_LOGIN_NAME"/>
+;; sample use:
+;; (get-xml-value-from-id "C:\\Users\\dexters\\AppData\\Roaming\\Toon Boom Animation\\localpref.xml" "LAST_LOGIN_NAME")
+(define (get-xml-value-from-id file pattern)
+  ; read file to lines
+  (define prefs-lines
+    (file->lines file))
+  ; helper func to find login line
+  (define (login line)
+    (string-contains? line pattern))
+  ; helper func to extract login alone
+  (define (clean line)
+    (define parts (string-split line "\""))
+    (second parts))
+  ; build list of matches and return unique match if found
+  (define matches (filter login prefs-lines))
+  (if (= (length matches) 1)
+      (clean (first matches))
+      #f))
+      
+      
+; EOF
